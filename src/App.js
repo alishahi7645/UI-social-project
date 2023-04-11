90dfc7935bddcc3fcf97c469a38b25c821ad6173
alishahi <shahy8227@gmail.com>
2023-04-11 06:40:45 -0700

    Preparation of project base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
+function App() {
+  return (
+    <main>
+        <header className="header-bg">
+          <Navbar />
+          <Header />
+        </header>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <div>
+    نوبار سایت
+  </div>;
+};
+
+export default Navbar;
