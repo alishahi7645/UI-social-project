5cb6187f444f0dde4b7ac628ffd4be533903c6fb
alishahi <shahy8227@gmail.com>
2023-04-12 11:43:31 -0700

    end styling header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import phoneheader from '../../assets/phone-header-bg.png'
+import phoneheader from '../../assets/phone-header-bg.png';
+import {BsMouse} from 'react-icons/bs'
 import './Header.css'
 const Header = () => {
   return (
@@ -11,7 +12,7 @@ const Header = () => {
             <span>امنیت در همه پلتفرم ها</span>
             <span>سیستم پیام رسانی</span>
           </h1>
-          <small>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ . با استافاده از </small>
+          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ . با استافاده از </p>
           <div className="header-btn">
             <a href="" className='btn btn-dark'>بیاین شروع کنید</a>
             <a href="" className='btn btn-light'>چجوری کار میکنیم</a>
@@ -20,6 +21,11 @@ const Header = () => {
         <div className="header-left">
           <img src={phoneheader} alt="" srcset="" />
         </div>
+      </div>
+      <div className="floating-icon">
+        <a href="">
+          <BsMouse color={'#fff'} size={25} className='mouse'/>
+        </a>
       </div>
     </section>
   )
