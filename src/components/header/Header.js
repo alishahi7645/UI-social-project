b3783527193b020c8336a5352b5ad46922b0b3fc
alishahi <shahy8227@gmail.com>
2023-04-12 02:20:13 -0700

    start design header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import phoneheader from '../../assets/phone-header-bg.png'
+import './Header.css'
 const Header = () => {
-  return <div>
-    هدر سایت
-  </div>;
+  return (
+    <section id="header">
+      <div className="container header">
+        <div className="header-right">
+          <h1>
+            <span>پیشرو در سطح جهانی</span>
+            <span>امنیت در همه پلتفرم ها</span>
+            <span>سیستم پیام رسانی</span>
+          </h1>
+          <small>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ . با استافاده از </small>
+          <div className="header-btn">
+            <a href="" className='btn btn-dark'>بیاین شروع کنید</a>
+            <a href="" className='btn btn-light'>چجوری کار میکنیم</a>
+          </div>
+        </div>
+        <div className="header-left">
+          <img src={phoneheader} alt="" srcset="" />
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Header;
