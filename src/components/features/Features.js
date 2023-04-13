f3ee0cdaa482c62081e57fb26d048bb8cb2fe381
alishahi <shahy8227@gmail.com>
2023-04-13 00:46:06 -0700

    start designed features

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import { BsFillBookmarkStarFill } from "react-icons/bs";
+import './Features.css'
 
 const Features = () => {
-  return <div></div>;
+  return (
+    <section id="features">
+      <div className="container features">
+        <div className="title">
+          <BsFillBookmarkStarFill color="orangered" size={"25"} />
+          <h2>ویزگی های اصلی</h2>
+          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+        </div>
+        <div className="features-content"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
