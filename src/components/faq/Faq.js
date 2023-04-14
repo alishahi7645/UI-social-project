eb3625b834e215df335d91974dfc6564e161cc6b
alishahi <shahy8227@gmail.com>
2023-04-14 11:13:01 -0700

    end styling faq

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineLibraryBooks } from "react-icons/md";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { questions } from "./data";
+import "./Faq.css";
 function Faq() {
+  const [showAnswer, setShowAnswer] = useState(false);
+  const toggleanswer = () => {
+    setShowAnswer(!showAnswer);
+  };
   return (
     <section id="faq">
       <div className="container faq">
@@ -15,21 +20,21 @@ function Faq() {
           </p>
         </div>
         <div className="questions">
-          {
-            questions && questions.map((question)=>
-              <div className="question" key={question.id}>
-              <div className="question-title">
-                <h4>{question.title}</h4>
-                <button>
-                  <AiOutlinePlus />
-                </button>
+          {questions.map((question) => {
+            return (
+              <div className="container question" key={question.id}>
+                <div className="question-title">
+                  <h3>{question.title}</h3>
+                  <button onClick={() => setShowAnswer(!showAnswer)}>
+                    {showAnswer ? <AiOutlineMinus color="#1f93ff" /> : <AiOutlinePlus color="#1f93ff" />}
+                  </button>
+                </div>
+                <div className="question-answer">
+                  {showAnswer && <p>{question.answe}</p>}
+                </div>
               </div>
-              <div className="question-answer">
-                <p>{question.answe}</p>
-              </div>
-            </div>
-            )
-          }
+            );
+          })}
         </div>
       </div>
     </section>
