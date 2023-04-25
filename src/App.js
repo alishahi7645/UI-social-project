4e71f2bb4b4c4f869b6629cc6d6ae4a9d16ea581
alishahi <shahy8227@gmail.com>
2023-04-25 13:12:54 -0700

    end create back to top button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import {Navbar,Header,Features,Download,Subscribe,Faq,Footer} from './components/index';
 import {FaChevronUp} from 'react-icons/fa';
 function App() {
+  const [showbtn , setShowBtn] = useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll', function(){
+      if(this.window.pageYOffset > 400){
+        setShowBtn(!showbtn)
+      }else{
+        setShowBtn(false)
+      }
+    })
+  },[])
   return (
     <main>
       <header className="header-bg">
@@ -12,7 +23,7 @@ function App() {
       <Subscribe />
       <Faq />
       <Footer />
-      <a href="#">
+      <a href="#" className={showbtn ? 'to-top active' : 'to-top'}>
         <FaChevronUp/>
       </a>
     </main>
